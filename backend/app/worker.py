ea68e5222940c5b07b8430e2d6bab95a82294ff1
Anwarshaikk <148291002+Anwarshaikk@users.noreply.github.com>
2025-06-14 07:46:53 -0400

    updated worker.py

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -1,6 +1,8 @@
 import os
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
+from rq.connections import Connection
+
 
 redis_url = os.getenv("REDIS_URL", "redis://redis:6379/0")
 conn = Redis.from_url(redis_url)
